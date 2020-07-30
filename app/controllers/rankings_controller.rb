eb88b339d056acf83e922ff397a47624115b528d
Maioon-png <mi_mkb.12_5@icloud.com>
2020-07-30 21:42:03 +0900

    modify getting data limit

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,5 @@
 class RankingsController < ApplicationController
   def index 
-    @records = Record.includes(:user).order("time DESC")
+    @records = Record.includes(:user).order("time DESC").limit(3)
   end
 end
