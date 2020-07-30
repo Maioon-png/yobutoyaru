f8491062f532c98baa25eaac7f0c592e1227e35c
Maioon-png <mi_mkb.12_5@icloud.com>
2020-07-30 20:17:46 +0900

    add  association　in user.rb & record.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   has_one :peer
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :record
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
   end
 end
